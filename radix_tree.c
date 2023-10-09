c1789ad4b58637cf8c88ddb18a28a62ec5e0f659
alexandreantonaccio <93547375+alexandreantonaccio@users.noreply.github.com>
2023-10-08 23:09:02 -0400

    remocao
    remocao

--- radix_tree.c
+++ radix_tree.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 
 #define ALPHABET_SIZE 26
 
-// Structure representing a node in the radix tree
-struct Node {
-    struct Node* children[ALPHABET_SIZE];
-    int isEndOfWord;
+struct RadixNode {
+    struct RadixNode* children[ALPHABET_SIZE];
+    bool isEndOfWord;
 };
 
-// Function to create a new node
-struct Node* createNode() {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+typedef struct RadixNode RadixNode;
+
+RadixNode* createNode() {
+    RadixNode* newNode = (RadixNode*)malloc(sizeof(RadixNode));
     if (newNode) {
-        newNode->isEndOfWord = 0;
+        newNode->isEndOfWord = false;
         for (int i = 0; i < ALPHABET_SIZE; i++) {
             newNode->children[i] = NULL;
         }
@@ -22,46 +23,132 @@ struct Node* createNode() {
     return newNode;
 }
 
-// Function to insert a string into the radix tree
-void insert(struct Node* root, const char* key) {
-    struct Node* currentNode = root;
-    for (int i = 0; i < strlen(key); i++) {
-        int index = key[i] - 'a';
-        if (!currentNode->children[index]) {
-            currentNode->children[index] = createNode();
-        }
-        currentNode = currentNode->children[index];
+RadixNode* insert(RadixNode* root, const char* key) {
+    if (root == NULL) {
+        root = createNode();
     }
-    currentNode->isEndOfWord = 1;
+
+    if (*key == '\0') {
+        root->isEndOfWord = true;
+    } else {
+        int index = *key - 'a';
+        root->children[index] = insert(root->children[index], key + 1);
+    }
+
+    return root;
 }
 
-// Function to search for a string in the radix tree
-int search(struct Node* root, const char* key) {
-    struct Node* currentNode = root;
-    for (int i = 0; i < strlen(key); i++) {
-        int index = key[i] - 'a';
-        if (!currentNode->children[index]) {
-            return 0; // Key not found
-        }
-        currentNode = currentNode->children[index];
+bool search(RadixNode* root, const char* key) {
+    if (root == NULL) {
+        return false;
     }
-    return currentNode->isEndOfWord; // Key found if isEndOfWord is true
+
+    if (*key == '\0') {
+        return root->isEndOfWord;
+    } else {
+        int index = *key - 'a';
+        return search(root->children[index], key + 1);
+    }
+}
+
+bool isNodeEmpty(RadixNode* node) {
+    for (int i = 0; i < ALPHABET_SIZE; i++) {
+        if (node->children[i] != NULL) {
+            return false;
+        }
+    }
+    return true;
+}
+
+RadixNode* removeKey(RadixNode* root, const char* key) {
+    if (root == NULL) {
+        return NULL;
+    }
+
+    if (*key == '\0') {
+        if (root->isEndOfWord) {
+            root->isEndOfWord = false;
+        }
+
+        if (isNodeEmpty(root)) {
+            free(root);
+            return NULL;
+        }
+
+        return root;
+    }
+
+    int index = *key - 'a';
+    root->children[index] = removeKey(root->children[index], key + 1);
+
+    if (isNodeEmpty(root) && !root->isEndOfWord) {
+        free(root);
+        return NULL;
+    }
+
+    return root;
+}
+
+void printTreeHelper(RadixNode* node, char* buffer, int depth) {
+    if (node == NULL) {
+        return;
+    }
+
+    if (node->isEndOfWord) {
+        buffer[depth] = '\0';
+        printf("%s\n", buffer);
+    }
+
+    for (int i = 0; i < ALPHABET_SIZE; i++) {
+        if (node->children[i] != NULL) {
+            buffer[depth] = i + 'a';
+            printTreeHelper(node->children[i], buffer, depth + 1);
+        }
+    }
+}
+
+void printTree(RadixNode* root) {
+    char buffer[256]; // Assuming a maximum key length of 255
+    printTreeHelper(root, buffer, 0);
+}
+
+void freeTree(RadixNode* root) {
+    if (root) {
+        for (int i = 0; i < ALPHABET_SIZE; i++) {
+            freeTree(root->children[i]);
+        }
+        free(root);
+    }
 }
 
 int main() {
-    struct Node* root = createNode();
+    RadixNode* root = NULL;
 
-    // Insert some strings
-    insert(root, "apple");
-    insert(root, "app");
-    insert(root, "banana");
+    // Insert keys
+    root = insert(root, "apple");
+    root = insert(root, "app");
+    root = insert(root, "banana");
 
-    // Search for strings
-    printf("Search for 'apple': %s\n", search(root, "apple") ? "Found" : "Not found");
-    printf("Search for 'app': %s\n", search(root, "app") ? "Found" : "Not found");
-    printf("Search for 'banana': %s\n", search(root, "banana") ? "Found" : "Not found");
-    printf("Search for 'cherry': %s\n", search(root, "cherry") ? "Found" : "Not found");
+    // Search for keys
+    printf("Search for 'apple': %s\n", search(root, "apple") ? "Found" : "Not Found");
+    printf("Search for 'app': %s\n", search(root, "app") ? "Found" : "Not Found");
+    printf("Search for 'banana': %s\n", search(root, "banana") ? "Found" : "Not Found");
+    printf("Search for 'cherry': %s\n", search(root, "cherry") ? "Found" : "Not Found");
 
-    // Clean up the memory (not shown here, but you should free the allocated memory)
+    // Print the tree
+    printf("Radix Tree:\n");
+    printTree(root);
+
+    // Remove keys
+    root = removeKey(root, "apple");
+    root = removeKey(root, "app");
+
+    // Search again after removal
+    printf("Search for 'apple' after removal: %s\n", search(root, "apple") ? "Found" : "Not Found");
+    printf("Search for 'app' after removal: %s\n", search(root, "app") ? "Found" : "Not Found");
+
+    // Free memory
+    freeTree(root);
+
     return 0;
 }
