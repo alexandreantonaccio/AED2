fdf5ef3dbabc74692e1418e65143dc3a5df63ba3
alexandreantonaccio <93547375+alexandreantonaccio@users.noreply.github.com>
2024-08-28 17:34:50 -0400

    aula_pratica1
    aula pratica + projeto 1 inicio

---
+++ trabalho1_cesar/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+typedef struct livro {
+    int key;
+    char titulo[50];
+    char autor[40];
+    int ano;
+} tipoLivro;
+
+void adicionaLivro(){
+    
+}
+
+void buscaLivro(){
+
+}
