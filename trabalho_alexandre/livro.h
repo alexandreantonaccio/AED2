411da4502e038f937a740403c29a1b5c321f4ffe
alexandreantonaccio <93547375+alexandreantonaccio@users.noreply.github.com>
2024-09-24 20:32:51 -0400

    .
    .

--- trabalho_alexandre/livro.h
+++
@@ -1,9 +0,0 @@
-#include "estrutura.c"
-
-Livro* criarLivro(char* isbn, char* titulo, char* autor, char* ano);
-void imprimirLivro( Livro* livro, int numLivros);
-void inserirLivro( Livro** lista, char* isbn, char* titulo, char* autor, char* ano);
-void buscarLivro( Livro* lista, char* chave);
-void checkInLivro( Livro* lista, char* chave);
-void checkOutLivro( Livro* lista, char* chave);
-void impimirBiblioteca( Livro* lista);
